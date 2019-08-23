537fb8fc5d84def60c690f39985a070a136ec4a2
Daniel Hervás <dhervas@outlook.es>
2019-08-23 17:56:56 +0200

    first working scraper/NLP version (ES disabled)

---
+++ script.py
@@ -0,0 +1,114 @@
+import newspaper
+import re
+from datetime import datetime
+from elasticsearch import Elasticsearch
+import spacy
+
+webs = [
+    'https://www.abqjournal.com/category/news-more',
+    'https://www.bostonglobe.com/world/',
+    # 'https://www.bostonherald.com/news/world-news/',
+    # 'https://www.charlotteobserver.com/news/nation-world/world/',
+    # 'https://www.cleveland.com/world/',
+    # 'https://www.star-telegram.com/news/nation-world/world',
+    # 'https://www.chron.com/news/nation-world/world/',
+    # 'https://www.kansascity.com/news/nation-world/world',
+    # 'https://www.miamiherald.com/news/nation-world/world/',
+    # 'http://www.startribune.com/world/',
+    # 'https://www.nytimes.com/section/world',
+    # 'https://oklahoman.com/news/us-world',
+    # 'https://www.post-gazette.com/news/world',
+    # 'https://www.sacbee.com/news/nation-world/world/',
+    # 'https://www.mysanantonio.com/news/us-world/world/',
+    # 'https://www.sfgate.com/world/',
+    # 'https://www.wsj.com/news/world',
+    # 'https://www.washingtonpost.com/world/',
+    # 'https://www.washingtontimes.com/news/world/',
+]
+
+def startElasticSearch():
+    body_settings = {
+        "settings": {
+            "number_of_shards": 1,
+            "number_of_replicas": 0
+        },
+        "mappings": {
+            "members": {
+                "dynamic": "strict",
+                "properties": {
+                    "newspaper_name": {
+                        "type": "text"
+                    },
+                    "url": {
+                        "type": "text"
+                    },
+                    "publication_date": {
+                        "type": "date"
+                    },
+                    "collection_date": {
+                        "type": "date"
+                    },
+                    "headline": {
+                        "type": "text"
+                    },
+                    "body": {
+                        "type": "text"
+                    },
+                    "tokens": {
+                        "type": "keyword"
+                    }
+                }
+            }
+        }
+    }
+    # es = Elasticsearch()
+    # es.indices.create(index="articles", ignore=400, body=body_settings)
+
+def processArticle(article):
+    article.download()
+    article.parse()
+    doc = nlp(article.text)
+    tokens = list(map(lambda y: y.lemma_, filter(lambda x: x.pos_ == "NOUN", doc)))
+    for token in tokens:
+        if (token in token_count):
+            token_count[token] = token_count[token] + 1
+        else:
+            token_count[token] = 0
+    body = {
+        "newspaper_name": paper.brand,
+        "url": article.url,
+        "publication_date": article.publish_date,
+        "collection_date": datetime.now(),
+        "headline": article.title,
+        "body": article.text,
+        "tokens": tokens
+    }
+    print(body)
+    # es.index(index='articles', body)
+
+
+class Category(object):
+    def __init__(self, url):
+        self.url = url
+        self.html = None
+        self.doc = None
+
+startElasticSearch()
+nlp = spacy.load("en_core_web_sm")
+token_count = {}
+for web in webs:
+    # paper = newspaper.build(web, language='en', memoize_articles = False)
+    paper = newspaper.Source(web, language='en', memoize_articles = False)
+    paper.categories = [Category(web)]
+    paper.download_categories()
+    paper.parse_categories()
+    paper.generate_articles()
+    brand = paper.brand
+    size = str(paper.size())
+    print (paper.size())
+    print ('Newspaper ' + brand + ' built. Size: ' + size)
+    if (paper.size() < 10):
+        print('Size too low. Exiting...')
+        break
+    for i in range (0, 2):
+        processArticle(paper.articles[i])
